21fca31d1c58604b883086b61d41910a1993939d
Tom Hofkens <tom.hofkens@gmail.com>
2025-01-18 22:02:48 +0100

    zorg dat die merge doet bij pull

--- lib/update.py
+++ lib/update.py
@@ -5,6 +5,7 @@ import os
 os.system("git add .")
 os.system('git commit -m "Mijn oplossingen"')
 os.system("git checkout main")
+os.system("git config pull.rebase false")
 os.system("git pull origin main")
 os.system("git checkout mijn-oplossingen")
 os.system("git merge main")
