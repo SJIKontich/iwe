5912b4f0e69a6d751aa71fdeb5a8ffaec7503963
Tom Hofkens <tom.hofkens@gmail.com>
2025-02-05 17:39:43 +0100

    oplossing

--- reeks2/vraag18.py
+++ reeks2/vraag18.py
@@ -10,3 +10,8 @@
 # v1[0]*v2[0] + v1[1]*v2[1] + ... + v1[n]*v2[n]
 
 # Zet hier je code
+def scalairproduct(v1, v2):
+    product = 0
+    for i in range(len(v1)):
+        product += v1[i] * v2[i]
+    return product
