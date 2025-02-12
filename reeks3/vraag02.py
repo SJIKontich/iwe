2888dc2e4c9d6369da5f93744975132b320f17a1
Tom Hofkens <tom.hofkens@gmail.com>
2025-02-12 17:29:56 +0100

    vraag2 ok

--- reeks3/vraag02.py
+++ reeks3/vraag02.py
@@ -1,11 +1,24 @@
 
-# Nulmatrix maken
+# Zoek de fout in de volgende code:
 
-# Schrijf een functie nulmatrix(aantalrijen,aantalkolommen) om een nulmatrix te maken.
-#
-# Doe dit in stappen (elke stap heeft een extra test). Probeer met een matrix met
-# - 1 rij en 1 kolom en roep de functie nulvector op van de vorige vraag
-# - 1 rij en 3 kolommen
-# - 2 rijen en 3 kolommen
+def som_positieve_getallen(getallen):
+    totaal = 0
+    for getal in getallen:
+        if getal > 0:
+            totaal += getal
+        else:
+            totaal = 0
+    return totaal
 
-# Zet hier je code
+# De functie zou de som van alle positieve getallen in de lijst moeten teruggeven.
+# Test de functie met verschillende lijsten en zoek de fout.
+
+# 1. Wat verwacht je dat deze code doet?
+
+
+
+# 2. Schrijf een test die deze verwachting verifieert.
+
+# 3. Voeg nu een extra assert toe aan de test_som_positieve_getallen functie en zorg dat die faalt.
+
+# 4. Gebruik de debugger om de fout te vinden en de test te laten slagen.
