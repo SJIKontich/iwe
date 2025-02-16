6581b27091a6f7615b7963b6368718eedef4716b
Tom Hofkens <tom.hofkens@gmail.com>
2025-02-16 16:10:35 +0100

    hint1

--- reeks3/vraag05.py
+++ reeks3/vraag05.py
@@ -9,5 +9,9 @@ def som_van_reeks(n):
 # De functie zou de som van alle getallen van 1 tot en met n moeten teruggeven.
 
 # 1. Schrijf een test die deze verwachting verifieert. Je zal merken dat de test faalt.
-
+def test_som_van_reeks():
+    assert som_van_reeks(1) == 1
+    assert som_van_reeks(2) == 3
+    assert som_van_reeks(3) == 6
+    
 # 2. Gebruik de debugger om de fout te vinden en de test te laten slagen.
