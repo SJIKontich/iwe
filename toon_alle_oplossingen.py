74e1a8960444d2ad1bceba5f0786b6c019b88cc6
Tom Hofkens <tom.hofkens@gmail.com>
2025-02-23 20:13:58 +0100

    als ze iets wijzigen aan oplossing branch wordt dat lokaal bijgehouden in de lokale repo

--- toon_alle_oplossingen.py
+++
@@ -1,9 +0,0 @@
-# python
-import os
-
-# Check if the current branch is 'mijn-oplossingen'
-current_branch = os.popen("git branch --show-current").read().strip()
-if current_branch == "mijn-oplossingen":
-    os.system("git add .")
-    os.system("git commit -m 'commit mijn oplossingen en switch naar alle oplossingen'")
-    os.system("git checkout oplossing")
--- toon_mijn_oplossingen.py
+++ toon_mijn_oplossingen.py
@@ -4,4 +4,6 @@ import os
 # Check if the current branch is 'mijn-oplossingen'
 current_branch = os.popen("git branch --show-current").read().strip()
 if current_branch == "oplossing":
+    os.system('git add .')
+    os.system('git commit -m "aanpassing aan alle oplossingen"')
     os.system("git checkout mijn-oplossingen") # als ze iets hebben aangepast, krijgen ze een error
