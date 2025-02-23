e5090f553d38687ab674e15c20e54a3fce6ee8bf
Tom Hofkens <tom.hofkens@gmail.com>
2025-02-23 20:12:10 +0100

    verwijderd

--- toon_mijn_oplossingen.py
+++
@@ -1,7 +0,0 @@
-# python
-import os
-
-# Check if the current branch is 'mijn-oplossingen'
-current_branch = os.popen("git branch --show-current").read().strip()
-if current_branch == "oplossing":
-    os.system("git checkout mijn-oplossingen") # als ze iets hebben aangepast, krijgen ze een error
